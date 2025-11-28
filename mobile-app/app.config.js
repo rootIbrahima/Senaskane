ca63ae85109e3c75ea81985974624bb5f5c14042
Ibrahima <lyibrahima321@gmail.com>
2025-11-28 12:39:21 +0000

    Fix: Garder le slug EAS original pour éviter conflit project ID

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: "Baïla Généa",
-    slug: "bailagenea",
+    slug: "senaskane",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
